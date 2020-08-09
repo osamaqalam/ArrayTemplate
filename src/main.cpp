a6357b8d38925ec945a2f5e50aed7986d7531c20
Osama <osama.qalam@hotmail.com>
2020-08-09 17:48:12 -0400

    output results to a seperate file

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 
 #include <iostream>
+#include <fstream>
 #include <string>
 using namespace std;
 
@@ -24,4 +25,19 @@ int main()
 	cin >> doubleArray;
 	cout << "Please enter 3 strings" << endl;
 	cin >> stringArray;
+
+	// Create and open a text file
+	ofstream MyFile("Template.txt");
+
+	MyFile << "The integers inputted are: ";
+	for (size_t i=0; i<intArray.getSize(); i++)
+		MyFile << intArray[i] << (i!=intArray.getSize()-1 ? "," : "\n");
+
+	MyFile << "The doubles inputted are: ";
+	for (size_t i=0; i<doubleArray.getSize(); i++)
+		MyFile << doubleArray[i] << (i!=doubleArray.getSize()-1 ? "," : "\n");
+
+	MyFile << "The strings inputted are: ";
+	for (size_t i=0; i<stringArray.getSize(); i++)
+		MyFile << stringArray[i] << (i!=stringArray.getSize()-1 ? "," : "\n");
 }
